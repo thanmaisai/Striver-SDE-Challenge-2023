9e521d79c0957a3f8890c7fb3b1df9b1555b715d
thanmaisai <91474486+thanmaisai@users.noreply.github.com>
2023-06-18 22:53:42 +0530

    Update 35_Linked_List_Cycle_II.java

--- 35_Linked_List_Cycle_II.java
+++ 35_Linked_List_Cycle_II.java
@@ -1,3 +1,19 @@
+// approach1: we can use a hashtable ans store each node occurance in the has table and once a LL is completely iterated and if we find a node which is 
+// the start of the cycle the then as that node will be present in the hash table we will just return node.
+// if we dont find the node and reach null then there is no cycle and return null;
+// Time Complexity: O(N)
+// Space Complexity: O(N) - for storing in hash table
+
+
+// approach2: here we are using the tortoise method where we we use slow fast and entry node assigned to head
+// here the fast move by next.next (2 steps) and slow move by next(1 step)
+// there will be some point where the slow and fast meet 
+// then from there until slow and entry meet keep incrementing both by next(one step) (entry start from head and slow start from the point where slow and fast meet)
+// finally when both meet then return entry
+// if not met then return null
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
 
 public class Solution {
     public static Node firstNode(Node head) {
