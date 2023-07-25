2260d69e1e20f39d3ac4c5518aad5db309caa15f
thanmaisai <91474486+thanmaisai@users.noreply.github.com>
2023-07-25 22:39:03 +0530

    Update Combination_Sum.java

--- Combination_Sum.java
+++ Combination_Sum.java
@@ -1,3 +1,12 @@
+// Time Complexity: O(2^t * k) where t is the target, k is the average length
+// Reason: Assume if you were not allowed to pick a single element multiple times, 
+// every element will have a couple of options: pick or not pick which is 2^n different recursion calls, 
+// also assuming that the average length of every combination generated is k. 
+// (to put length k data structure into another data structure)
+// Why not (2^n) but (2^t) (where n is the size of an array)?
+// Assume that there is 1 and the target you want to reach is 10 so 10 times you can “pick or not pick” an element.
+// Space Complexity: O(k*x), k is the average length and x is the no. of combinations
+
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List < List < Integer >> ans = new ArrayList < > ();
